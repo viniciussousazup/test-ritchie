5a1f551df5eddcee281a65412f809e1d5278021d
Lucas Sales <lucas.sales@zup.com.br>
2021-03-18 17:20:34 -0300

    add steps

--- test/kotlintemplate/src/pkg/formula/formula.go
+++ test/kotlintemplate/src/pkg/formula/formula.go
@@ -4,12 +4,8 @@
 package formula
 
 import (
-	"context"
 	"fmt"
 	"time"
-
-	"github.com/google/go-github/v33/github"
-	"golang.org/x/oauth2"
 )
 
 type Formula struct {
@@ -17,21 +13,23 @@ type Formula struct {
 }
 
 func (f Formula) Run() {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: f.Token},
-	)
-	tc := oauth2.NewClient(ctx, ts)
+	// ctx := context.Background()
+	// ts := oauth2.StaticTokenSource(
+	// 	&oauth2.Token{AccessToken: f.Token},
+	// )
+	// tc := oauth2.NewClient(ctx, ts)
 
-	client := github.NewClient(tc)
+	// client := github.NewClient(tc)
 
-	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List(ctx, "", nil)
-	if err != nil {
-		fmt.Println("list repos", err)
-	}
-	for _, repository := range repos {
-		time.Sleep(30 * time.Second)
-		fmt.Println(*repository.Name)
+	// // list all repositories for the authenticated user
+	// repos, _, err := client.Repositories.List(ctx, "", nil)
+	// if err != nil {
+	// 	fmt.Println("list repos", err)
+	// }
+	test := 1
+	for ok := true; ok; ok = test < 7 {
+		fmt.Printf("STEP %d \n", test)
+		time.Sleep(20 * time.Second)
+		test++
 	}
 }
